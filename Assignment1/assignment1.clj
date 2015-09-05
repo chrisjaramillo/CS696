6079b0ab249f64a61e64daf3bb5d2dc9aded9969
Christopher Jaramillo <chris.jaramillo@gmail.com>
2015-09-05 13:56:54 -0700

    Fix for question 1

--- Assignment1/assignment1.clj
+++ Assignment1/assignment1.clj
@@ -1,3 +1,3 @@
 ; Question one
-(defn bill-total [coll] (let [[x & rest] coll] (if (empty? rest) (* (x :amount) (x :quantity)) (+ (* (x :amount) (x :quantity)) (ader rest)))) )
+(defn bill-total [coll] (let [[x & rest] coll] (if (empty? rest) (* (x :amount) (x :quantity)) (+ (* (x :amount) (x :quantity)) (bill-total rest)))) )
 ; Question two
