c8a2e52772a688133e3a5e7c6d21d4d47f336326
Christopher Jaramillo <christopherjaramillo@Christophers-MacBook-Pro.local>
2015-09-06 05:31:01 -0700

    Uppercase test check in

--- Assignment1/assignment1.clj
+++ Assignment1/assignment1.clj
@@ -1,13 +1,13 @@
-; Question one
+; Question One
 (defn bill-total [coll] (let [[x & rest] coll] (if (empty? rest) (* (x :amount) (x :quantity)) (+ (* (x :amount) (x :quantity)) (bill-total rest)))) )
-; Question two
+; Question Two
 (defn add-to-bill [bill items] (into bill items))
-; Question three
+; Question Three
 (defn make-poly [coll] (let [[two one zero] coll] #(+ (* (first two)(Math/pow % (second two))) (* (first one)(Math/pow % (second one))) (* (first zero)(Math/pow % (second zero))))))
-; Question four
+; Question Four
 (defn deriv [x] [(* (first x) (second x)) (- (second x) 1)])
 (defn differentiate [x] (let [v (filter #(not= (second %) 0) x)](map deriv v)))
-; Question five
-; Question six
+; Question Five
+; Question Six
 (defn withdraw [x y] (update-in x [:balance] - y))
 (defn deposit [x y] (update-in x [:balance] + y))
