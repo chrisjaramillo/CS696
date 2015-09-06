474ce51d47ec206661350b438af3a7ea350b0f1d
Christopher Jaramillo <chris.jaramillo@gmail.com>
2015-09-05 18:04:25 -0700

    Done with three

--- Assignment1/assignment1.clj
+++ Assignment1/assignment1.clj
@@ -1,3 +1,7 @@
 ; Question one
 (defn bill-total [coll] (let [[x & rest] coll] (if (empty? rest) (* (x :amount) (x :quantity)) (+ (* (x :amount) (x :quantity)) (bill-total rest)))) )
 ; Question two
+(defn add-to-bill [bill items] (into bill items))
+; Question three
+(defn make-poly [coll] (let [[two one zero] coll] #(+ (* (first two)(Math/pow % (second two))) (* (first one)(Math/pow % (second one))) (* (first zero)(Math/pow % (second zero))))))
+; Question four
