a218f1e5b6b4163e516638ffabe18260b55f4736
Christopher Jaramillo <chris.jaramillo@gmail.com>
2015-09-05 19:02:27 -0700

    working

--- Assignment1/assignment1.clj
+++ Assignment1/assignment1.clj
@@ -5,3 +5,9 @@
 ; Question three
 (defn make-poly [coll] (let [[two one zero] coll] #(+ (* (first two)(Math/pow % (second two))) (* (first one)(Math/pow % (second one))) (* (first zero)(Math/pow % (second zero))))))
 ; Question four
+(defn deriv [x] [(* (first x) (second x)) (- (second x) 1)])
+(defn differentiate [x] (let [v (filter #(not= (second %) 0) x)](map deriv v)))
+; Question five
+; Question six
+(defn withdraw [x y] (update-in x [:balance] - y))
+(defn deposit [x y] (update-in x [:balance] + y))
