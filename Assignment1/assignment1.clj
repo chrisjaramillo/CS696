3d126d9015db55d866647a3fa967833c61f0575f
Christopher Jaramillo <christopherjaramillo@Christophers-MacBook-Pro.local>
2015-09-07 06:59:42 -0700

    Completed Assignment 1

--- Assignment1/assignment1.clj
+++ Assignment1/assignment1.clj
@@ -1,9 +1,9 @@
 ; Question One
 (defn bill-total [coll]
-  (let [[x & rest] coll]
+  (let [[item & rest] coll]
     (if (empty? rest)
-      (* (x :amount) (x :quantity))
-      (+ (* (x :amount) (x :quantity)) (bill-total rest)))))
+      (* (item :amount) (item :quantity))
+      (+ (* (item :amount) (item :quantity)) (bill-total rest)))))
 ; Question Two
 (defn add-to-bill [bill items]
   (into bill items))
@@ -17,12 +17,22 @@
   (let [poly-terms (poly-convert coll)]
     #(reduce + (for [poly poly-terms] (poly %)))))
 ; Question Four
-(defn deriv [x] [(* (first x) (second x)) (- (second x) 1)])
+(defn deriv [x]
+  (let [[a n] x][(* a n) (- n 1)]))
 (defn differentiate [x]
   (let [v (filter #(not= (second %) 0) x)]
     (map deriv v)))
 ; Question Five
-
+(defn calcx [eqn eqn' x]
+  (- x (/ (eqn x) (eqn' x))))
+(defn find-root [tolerance eqn guess]
+  (let [px (make-poly eqn) px' (make-poly (differentiate eqn))]
+  (let [x1 (calcx px px' guess)]
+  (if (<= (Math/abs (- guess x1)) tolerance)
+      (format "%.6f" guess)
+    (find-root tolerance eqn x1)))))
 ; Question Six
-(defn withdraw [x y] (update-in x [:balance] - y))
-(defn deposit [x y] (update-in x [:balance] + y))
+(defn withdraw [account amount]
+  (update-in account [:balance] - amount))
+(defn deposit [account amount]
+  (update-in account [:balance] + amount))
