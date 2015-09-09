106ddc8af88f88d782acd20be0cd0eb3b51c3190
Christopher Jaramillo <christopherjaramillo@Christophers-MacBook-Pro.local>
2015-09-09 07:00:11 -0700

    Final homework

--- Assignment1/assignment1.clj
+++ Assignment1/assignment1.clj
@@ -4,9 +4,12 @@
     (if (empty? rest)
       (* (item :amount) (item :quantity))
       (+ (* (item :amount) (item :quantity)) (bill-total rest)))))
+
 ; Question Two
 (defn update-bill [bill item index]
-  (assoc bill index (assoc-in (nth bill index) [:quantity] (+ (item :quantity) ((nth bill index ) :quantity)))))
+  (assoc bill index
+    (assoc-in (nth bill index) [:quantity]
+              (+ (item :quantity) ((nth bill index ) :quantity)))))
 (defn map-index-in-vec [xs newmap currindex]
   (if (>= currindex (count xs))
     nil
@@ -23,6 +26,7 @@
   (if (empty? rest)
     (bill-add item bill)
     (add-to-bill (bill-add item bill) rest))))
+
 ; Question Three
 (defn poly-term [coll]
   (let [[coef power]coll]
@@ -32,12 +36,14 @@
 (defn make-poly [coll]
   (let [poly-terms (poly-convert coll)]
     #(reduce + (for [poly poly-terms] (poly %)))))
+
 ; Question Four
 (defn deriv [x]
   (let [[a n] x][(* a n) (- n 1)]))
 (defn differentiate [x]
   (let [v (filter #(not= (second %) 0) x)]
     (map deriv v)))
+
 ; Question Five
 (defn calcx [eqn eqn' x]
   (- x (/ (eqn x) (eqn' x))))
@@ -47,6 +53,7 @@
   (if (<= (Math/abs (- guess x1)) tolerance)
       (format "%.6f" guess)
     (find-root tolerance eqn x1)))))
+
 ; Question Six
 (defn withdraw [account amount]
   (update-in account [:balance] - amount))
