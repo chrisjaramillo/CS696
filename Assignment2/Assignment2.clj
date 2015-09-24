48a92170c295ee2c6fa97fb2923d9a0bdc04ab35
Christopher Jaramillo <christopherjaramillo@Christophers-MacBook-Pro.local>
2015-09-24 04:47:32 -0700

    Done with more!

--- Assignment2/Assignment2.clj
+++ Assignment2/Assignment2.clj
@@ -7,7 +7,7 @@
 
 (defn divisors [x]
   (let[low (lowfactors x) high (highfactors low x)]
-    (sort (into high low))))
+    (distinct (sort (into high low)))))
 
 ;Question 2
 (defn abundance [x]
@@ -19,3 +19,17 @@
   (let [potential (range 1 (+ x 1))]
     (filter #(> (abundance %) 0) potential)))
 
+
+; Question 4
+(defn all-substrings-of-length [x length]
+  (let [result '()]
+    (when (>= (count x) length)
+    (into result
+          (into (list (subs x 0 length)) (all-substrings-of-length (subs x 1) length))))))
+
+(defn pattern-count [text pattern]
+  (let [substrings (all-substrings-of-length text (count pattern))
+        matches (filter #(= pattern %) substrings)]
+    (count matches)))
+
+; Question 5
